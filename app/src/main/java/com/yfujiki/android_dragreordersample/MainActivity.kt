603eb62dba262e241b2ce162f51aa9d5e6a84130
Yuichi Fujiki <yuichi@chaione.com>
2018-12-05 08:03:03 -0700

    Changed color when state became active.

--- app/src/main/java/com/yfujiki/android_dragreordersample/MainActivity.kt
+++ app/src/main/java/com/yfujiki/android_dragreordersample/MainActivity.kt
@@ -33,6 +33,20 @@ class MainActivity : AppCompatActivity() {
 
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
             }
+
+            override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+                super.onSelectedChanged(viewHolder, actionState)
+
+                if (actionState == ACTION_STATE_DRAG) {
+                    viewHolder?.itemView?.alpha = 0.5f
+                }
+            }
+
+            override fun clearView(recyclerView: RecyclerView, viewHolder: RecyclerView.ViewHolder) {
+                super.clearView(recyclerView, viewHolder)
+
+                viewHolder?.itemView?.alpha = 1.0f
+            }
         }
 
         ItemTouchHelper(simpleItemTouchCallback)
